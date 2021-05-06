c88262341c9b925de13bfdb12d4d7207cc7e47c8
Onkar Dahale <dahaleonkar@gmail.com>
2021-05-06 21:39:02 +0530

    Add files via upload
    refactored the code

--- MainApp.py
+++ MainApp.py
@@ -1,65 +1,45 @@
 
 import json
-import requests
-
+import requests
 
 #Get form url with PINCODE && DATE inputs from user
-baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?"
+baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?"
 #Spoof User-Agent to bypass restrictions
-headers_dict = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
+headers_dict = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
 
-	
 #Main interface for Pincode and Date input
-def main():
-    
-    	print("###########################################")
-	print("#        CoWin Availablity Bot            #")
-	print("###########################################")
+def main():
 
-	
+    print("###########################################")
+    print("#        CoWin Availablity Bot            #")
+    print("###########################################")
 
-	pincode = input("Enter pincode: ")
-	date = input("Enter date: ")
-    
-    
-    	url = baseUrl + "pincode=" + pincode + "&date=" + date
-
-	response = requests.get(url, headers=headers_dict)
-    
+    pincode = input("Enter pincode: ")
+    date = input("Enter date: ")
+    url = baseUrl + "pincode=" + pincode + "&date=" + date
+    response = requests.get(url, headers=headers_dict)
     #check for OK status
-    if response.status_code == 200:
-        jsonStr = json.loads(response.txt)
+    if response.status_code == 200:
+        jsonStr = json.loads(response.text)
         center_list = list(jsonStr['centers'])
-        valid_centers = availCenter(center_list)
-        
+        valid_centers = availCenter(center_list)
         #output available centers
-        if len(valid_centers) > 0:
+        if len(valid_centers) > 0:
             print("######################################")
             print("#        Available Centers           #")
-            print("######################################")
-            
-            print(valid_centers)
-        
-        else:
+            print("######################################")            
+            print(valid_centers)            
+        else:
             print("No center available")
-    else:
-        print("No response...")
-    
-    
-    
-
-
-#Processes JSON data to extract list of centers with available slot and with needed age preference
-def availCenter(centerList):
-    
-    Lvalidcenters = []
+    else:
+        print("No response...")
+#Processes JSON data to extract list of centers with available slot and with needed age preferences
+def availCenter(centerList):
+    Lvalidcenters = []
     for center in centerList:
         for session in center['sessions']:
             if session['min_age_limit'] == 18 and session['available_capacity'] > 0:
-                Lvalidcenters.append(center)
+                Lvalidcenters.append(center)
     
-    return Lvalidcenters
-    
-       
-    
-main()
+    return Lvalidcenters
+main()
