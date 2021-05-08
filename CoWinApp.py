efc2829907174d1ef168f31223eaaea914b96205
Onkar Dahale <dahaleonkar@gmail.com>
2021-05-08 13:09:59 +0530

    Add files via upload
    added alert sound using pygame

--- CoWinApp.py
+++ CoWinApp.py
@@ -3,9 +3,14 @@ import time
 import json, requests
 import pandas as pd
 
+#supress pygame support message
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+import pygame
 
 #Get form url with PINCODE && DATE inputs from user
-baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/?"
+baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calenderByPin?"
 #Spoof User-Agent to bypass restrictions
 headers_list = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
 
@@ -37,15 +42,15 @@ def cowinApp(pincode, date, ageLimit):
         valid_centers = availCenter(jsonStr, ageLimit)
 
         df = pd.DataFrame(valid_centers, columns=['name', 'address', 'fee_type'])
-        if len(df):
+        if len(df) > 0:
             print("###########################################")
             print("#          Available Centers              #")
             print("###########################################")
             print(df)
+
+            alertSound()
         else:
             print("No Available Centers")
-
-            # TODO playsound when centers found
     else:
         print("No response...")
         exit()
@@ -58,6 +63,13 @@ def availCenter(centerList , minAgeLimit):
             if session['min_age_limit'] == minAgeLimit and session['available_capacity'] > 0:
                 validcenters.append(center)
     return validcenters
+
+# plays alert sound
+def alertSound():
+    pygame.mixer.init()
+    my_sound = pygame.mixer.Sound('notifSound.wav')
+    my_sound.play()
+    pygame.time.wait(int(my_sound.get_length) * 1000)
 
 
 main()
