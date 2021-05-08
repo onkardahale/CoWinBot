d43b3a2b703f333027ae37ae8b930f1be466549f
Onkar Dahale <dahaleonkar@gmail.com>
2021-05-08 13:39:03 +0530

    Fixed baseUrl

--- CoWinApp.py
+++ CoWinApp.py
@@ -10,7 +10,7 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 
 #Get form url with PINCODE && DATE inputs from user
-baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calenderByPin?"
+baseUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?"
 
 #Spoof User-Agent to bypass restrictions
 headers_list = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
